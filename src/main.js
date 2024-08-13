5fe8ecf1628f97db88a2558c29e5a221b09338fa
Akio <aakocherga@mail.ru>
2024-08-13 16:00:46 +0700

    Added choice theme

--- src/main.js
+++ src/main.js
@@ -1,75 +1,238 @@
+const btnReverse = document.querySelector(".reverse-page")
+
 particlesJS("particles", {
-  "particles": {
-      "number": {
-          "value": 100,
-          "density": {
-              "enable": true,
-              "value_area": 800
-          }
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
       },
-      "color": {
-          "value": "#ffffff"
+      color: {
+        value: "#ffffff",
       },
-      "shape": {
-          "type": "circle",
-          "stroke": {
-              "width": 0,
-              "color": "#000000"
-          }
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
       },
-      "opacity": {
-          "value": 0.8,
-          "random": true,
-          "animation": {
-              "enable": true,
-              "speed": 1,
-              "opacity_min": 0,
-              "sync": false
-          }
+      opacity: {
+        value: 0.8,
+        random: true,
+        animation: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0,
+          sync: false,
+        },
       },
-      "size": {
-          "value": 3,
-          "random": true
+      size: {
+        value: 3,
+        random: true,
       },
-      "line_linked": {
-          "enable": true,
-          "distance": 150,
-          "color": "#ffffff",
-          "opacity": 0.4,
-          "width": 1
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
       },
-      "move": {
-          "enable": true,
-          "speed": 2,
-          "direction": "none",
-          "random": true,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-      }
-  },
-  "interactivity": {
-      "detectsOn": "canvas",
-      "events": {
-          "onhover": {
-              "enable": false,
-              "mode": "repulse"
-          },
-          "onclick": {
-              "enable": false,
-              "mode": "push"
-          },
-          "resize": true
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
       },
-      "modes": {
-          "repulse": {
-              "distance": 100,
-              "duration": 0.4
-          },
-          "push": {
-              "particles_nb": 4
-          }
-      }
-  },
-  "retina_detect": true
+    },
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onhover: {
+          enable: false,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: false,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        repulse: {
+          distance: 100,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+      },
+    },
+    retina_detect: true,
 });
+
+btnReverse.addEventListener("click", function() {
+  const main = document.querySelector(".main")
+  if (main.style.color == "white") {
+    main.style.backgroundColor = "white"
+    main.style.color = "black"
+    particlesJS("particles", {
+        particles: {
+          number: {
+            value: 100,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#b91c1c",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#b91c1c",
+            },
+          },
+          opacity: {
+            value: 0.8,
+            random: true,
+            animation: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#b91c1c",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+          },
+        },
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onhover: {
+              enable: false,
+              mode: "repulse",
+            },
+            onclick: {
+              enable: false,
+              mode: "push",
+            },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+          },
+        },
+        retina_detect: true,
+    });
+  } else {
+    main.style.backgroundColor = "rgb(20, 15, 31)"
+    main.style.color = "white"
+    particlesJS("particles", {
+        particles: {
+          number: {
+            value: 100,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#ffffff",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000",
+            },
+          },
+          opacity: {
+            value: 0.8,
+            random: true,
+            animation: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+          },
+        },
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onhover: {
+              enable: false,
+              mode: "repulse",
+            },
+            onclick: {
+              enable: false,
+              mode: "push",
+            },
+            resize: true,
+          },
+          modes: {
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+          },
+        },
+        retina_detect: true,
+    });
+  }
+})
